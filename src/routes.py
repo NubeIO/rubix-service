f5f7da4794f8b4f92b9b40b545378867bf5f5258
NubeDev <aidan@envrioservices.com.au>
2020-11-11 20:41:51 +1100

    tests

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,12 @@
 from flask_restful import Api
 from src import app
 from src.system.resources.memory import GetSystemMem
-from src.system.resources.systemctl_services import SystemctlStatus
+from src.system.resources.systemctl_services import SystemctlStatus, SystemctlCommand
 
 api_prefix = 'api'
 api = Api(app)
 
 
 api.add_resource(GetSystemMem, "/{}/system/memory".format(api_prefix))
+api.add_resource(SystemctlCommand, "/{}/system/service".format(api_prefix))
 api.add_resource(SystemctlStatus, "/{}/system/service/<string:service>".format(api_prefix))
