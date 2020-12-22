59f2e9d8439deea33d485398736b7ca2a1e875db
zero88 <sontt246@gmail.com>
2020-12-22 16:25:09 +0700

    - Add build.Dockerfile and build.alpine.Dockerfile for build only (without nodejs) - Add runtime.Dockerfile for including nodejs to test later

--- docker.sh
+++ docker.sh
@@ -4,15 +4,18 @@ source ./docker/.env
 
 sha=$(git rev-parse --short HEAD)
 tag="dev"
+mode=$1
+[[ -z $mode ]] && mode=runtime
+echo $mode
 
 DOCKER_BUILDKIT=1 docker build \
     --build-arg "MAINTAINER=$APP_MAINTAINER" \
     --build-arg "APP_VERSION=$APP_VERSION" \
-    --build-arg "PYTHON_VERSION=$PYTHON_VERSION" \
+    --build-arg "BASE_IMAGE_VERSION=$PYTHON_VERSION" \
     --build-arg "COMMIT_SHA=$sha" \
     -t rubix-service:"$tag"\
     --progress=plain \
-    -f "$(pwd)/docker/Dockerfile" \
+    -f "$(pwd)/docker/$mode.Dockerfile" \
     ./ || { echo "Build $tag failure"; exit 2; }
 
 docker rmi $(docker images | grep "none" | awk '/ / { print $3 }') || exit 0
