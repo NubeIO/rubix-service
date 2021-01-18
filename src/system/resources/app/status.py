8dea42901e0a624488de27ccc1d50c5641c2c48d
RaiBnod <binodrayee@gmail.com>
2021-01-18 13:41:56 +0545

    Add RubixService as installed apps

--- src/system/resources/app/status.py
+++ src/system/resources/app/status.py
@@ -2,7 +2,11 @@ from flask_restful import Resource, abort, fields, marshal_with
 
 from src.inheritors import inheritors
 from src.system.apps.base.installable_app import InstallableApp
+from src.system.apps.enums.types import Types
 from src.system.resources.app.utils import get_installed_app_details
+from src.system.systemd.systemd import RubixServiceSystemd
+from src.system.utils.project import get_version
+from src.system.utils.shell import systemctl_status
 
 
 class StatusResource(Resource):
@@ -29,14 +33,13 @@ class StatusResource(Resource):
     @classmethod
     def get_installed_apps(cls):
         installed_apps = []
-        # TODO: have this one after self update integration
-        # status = systemctl_status(RubixServiceSystemd.SERVICE_FILE_NAME)
-        # installed_apps.append({
-        #     'version': get_version(),
-        #     'display_name': 'Rubix Service',
-        #     'app_type': Types.INSTALLER.value,
-        #     **status
-        # })
+        status = systemctl_status(RubixServiceSystemd.SERVICE_FILE_NAME)
+        installed_apps.append({
+            'version': get_version(),
+            'display_name': 'Rubix Service',
+            'app_type': Types.INSTALLER.value,
+            **status
+        })
         for installable_app in inheritors(InstallableApp):
             dummy_app = installable_app()
             details = get_installed_app_details(dummy_app)
