e311007a42f1f0b1b738e3c65816c9b86dbe3b22
RaiBnod <binodrayee@gmail.com>
2021-02-16 09:15:17 +0545

    Change timezone list structure

--- src/system/resources/utils.py
+++ src/system/resources/utils.py
@@ -1,53 +1,53 @@
 def convert_float_decimal(f=0.0, precision=2):
-    '''
-        Convert a float to string of decimal.
-        precision: by default 2.
-        If no arg provided, return "0.00".
-        '''
+    """
+    Convert a float to string of decimal.
+    precision: by default 2.
+    If no arg provided, return "0.00".
+    """
     return ("%." + str(precision) + "f") % f
 
 
-def format_size(size, sizeIn, sizeOut, precision=0):
-    '''
-        Convert file/disc size to a string representing its value in B, KB, MB and GB.
-        The convention is based on sizeIn as original unit and sizeOut
-        as final unit.
-        '''
-    assert sizeIn.upper() in {"B", "KB", "MB", "GB"}, "sizeIn type error"
-    assert sizeOut.upper() in {"B", "KB", "MB", "GB"}, "sizeOut type error"
-    if sizeIn == "B":
-        if sizeOut == "KB":
+def format_size(size, size_in, size_out, precision=0):
+    """
+    Convert file/disc size to a string representing its value in B, KB, MB and GB.
+    The convention is based on size_in as original unit and size_out
+    as final unit.
+    """
+    assert size_in.upper() in {"B", "KB", "MB", "GB"}, "size_in type error"
+    assert size_out.upper() in {"B", "KB", "MB", "GB"}, "size_out type error"
+    if size_in == "B":
+        if size_out == "KB":
             return convert_float_decimal((size / 1024.0), precision)
-        elif sizeOut == "MB":
+        elif size_out == "MB":
             return convert_float_decimal((size / 1024.0 ** 2), precision)
-        elif sizeOut == "GB":
+        elif size_out == "GB":
             return convert_float_decimal((size / 1024.0 ** 3), precision)
-    elif sizeIn == "KB":
-        if sizeOut == "B":
+    elif size_in == "KB":
+        if size_out == "B":
             return convert_float_decimal((size * 1024.0), precision)
-        elif sizeOut == "MB":
+        elif size_out == "MB":
             return convert_float_decimal((size / 1024.0), precision)
-        elif sizeOut == "GB":
+        elif size_out == "GB":
             return convert_float_decimal((size / 1024.0 ** 2), precision)
-    elif sizeIn == "MB":
-        if sizeOut == "B":
+    elif size_in == "MB":
+        if size_out == "B":
             return convert_float_decimal((size * 1024.0 ** 2), precision)
-        elif sizeOut == "KB":
+        elif size_out == "KB":
             return convert_float_decimal((size * 1024.0), precision)
-        elif sizeOut == "GB":
+        elif size_out == "GB":
             return convert_float_decimal((size / 1024.0), precision)
-    elif sizeIn == "GB":
-        if sizeOut == "B":
+    elif size_in == "GB":
+        if size_out == "B":
             return convert_float_decimal((size * 1024.0 ** 3), precision)
-        elif sizeOut == "KB":
+        elif size_out == "KB":
             return convert_float_decimal((size * 1024.0 ** 2), precision)
-        elif sizeOut == "MB":
+        elif size_out == "MB":
             return convert_float_decimal((size * 1024.0), precision)
 
 
 def convert_bytes(num):
     """
-    this function will convert bytes to MB.... GB... etc
+    this function will convert bytes to MB... GB... etc
     """
     step_unit = 1000.0  # 1024 bad the size
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
