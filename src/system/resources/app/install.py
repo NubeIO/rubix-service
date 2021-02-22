afadec5518e87e7a372c5e923c091b0e81a06559
enjuthulung <enjuthulung@gmail.com>
2021-02-22 21:36:47 +0545

    Resolve conflict

--- src/system/resources/app/install.py
+++ src/system/resources/app/install.py
@@ -1,13 +1,15 @@
 import shutil
 
-from flask_restful import Resource, reqparse, abort
+from flask_restful import reqparse
+from rubix_http.exceptions.exception import NotFoundException
+from rubix_http.resource import RubixResource
 
 from src.system.apps.base.installable_app import InstallableApp
 from src.system.resources.app.utils import get_app_from_service
 from src.system.utils.file import is_dir_exist, delete_existing_folder
 
 
-class InstallResource(Resource):
+class InstallResource(RubixResource):
     @classmethod
     def post(cls):
         parser = reqparse.RequestParser()
@@ -18,13 +20,10 @@ class InstallResource(Resource):
         version: str = args['version']
         app: InstallableApp = get_app_from_service(service, version)
         if not is_dir_exist(app.get_downloaded_dir()):
-            abort(404, message=str('Please download service {} with version {} at first'.format(service, version)))
-        try:
-            backup_data: bool = app.backup_data()
-            delete_existing_folder(app.get_installation_dir())
-            shutil.copytree(app.get_downloaded_dir(), app.get_installed_dir())
-            installation: bool = app.install()
-            delete_existing_folder(app.get_download_dir())
-            return {'service': service, 'version': version, 'installation': installation, 'backup_data': backup_data}
-        except Exception as e:
-            abort(501, message=str(e))
+            raise NotFoundException(f'Please download service {service} with version {version} at first')
+        backup_data: bool = app.backup_data()
+        delete_existing_folder(app.get_installation_dir())
+        shutil.copytree(app.get_downloaded_dir(), app.get_installed_dir())
+        installation: bool = app.install()
+        delete_existing_folder(app.get_download_dir())
+        return {'service': service, 'version': version, 'installation': installation, 'backup_data': backup_data}
