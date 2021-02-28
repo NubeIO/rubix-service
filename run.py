0c84db7c6b50cb27c39d04279e4dbb8517aa4d88
Enju Thulung <enjuthulung@gmail.com>
2021-02-28 15:00:57 +0545

    Standardization of data convention (#111)

--- run.py
+++ run.py
@@ -16,10 +16,12 @@ def number_of_workers():
 
 @click.command(context_settings=CLI_CTX_SETTINGS)
 @click.option('-p', '--port', type=int, default=AppSetting.PORT, show_default=True, help='Port')
+@click.option('-g', '--global-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.GLOBAL_DIR_ENV),
+              help='Global dir')
 @click.option('-d', '--data-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.DATA_DIR_ENV),
               help='Application data dir')
-@click.option('-g', '--global-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.GLOBAL_DATA_DIR_ENV),
-              help='Global data dir')
+@click.option('-c', '--config-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.CONFIG_DIR_ENV),
+              help='Application config dir')
 @click.option('-a', '--artifact-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.ARTIFACT_DIR_ENV),
               help='Artifact downloaded dir')
 @click.option('-b', '--backup-dir', type=click.Path(), default=lambda: os.environ.get(AppSetting.BACKUP_DATA_DIR_ENV),
@@ -27,17 +29,17 @@ def number_of_workers():
 @click.option('--prod', is_flag=True, help='Production mode')
 @click.option('-s', '--setting-file', help='Rubix-Service: setting json file')
 @click.option('--workers', type=int, help='Gunicorn: The number of worker processes for handling requests.')
-@click.option('-c', '--gunicorn-config', help='Gunicorn: config file(gunicorn.conf.py)')
+@click.option('--gunicorn-config', help='Gunicorn: config file(gunicorn.conf.py)')
 @click.option('--log-level', type=click.Choice(['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'], case_sensitive=False),
               show_default=True, help='Logging level')
 @click.option('--device-type', type=click.Choice(['amd64', 'arm64', 'armv7']), default='armv7', show_default=True,
               help='Device type')
 @click.option('--auth', is_flag=True, help='Enable JWT authentication.')
 @click.option('-l', '--logging-conf', help='Rubix-Service: logging config file')
-def cli(port, data_dir, global_dir, artifact_dir, backup_dir, prod, workers, setting_file, gunicorn_config, log_level,
-        device_type, auth, logging_conf):
-    setting = AppSetting(global_dir=global_dir, data_dir=data_dir, artifact_dir=artifact_dir, backup_dir=backup_dir,
-                         prod=prod, device_type=device_type, auth=auth).reload(setting_file)
+def cli(port, global_dir, data_dir, config_dir, artifact_dir, backup_dir, prod, workers, setting_file, gunicorn_config,
+        log_level, device_type, auth, logging_conf):
+    setting = AppSetting(global_dir=global_dir, data_dir=data_dir, config_dir=config_dir, artifact_dir=artifact_dir,
+                         backup_dir=backup_dir, prod=prod, device_type=device_type, auth=auth).reload(setting_file)
     options = {
         'bind': '%s:%s' % ('0.0.0.0', port),
         'workers': workers if workers is not None else number_of_workers() if prod else 1,
