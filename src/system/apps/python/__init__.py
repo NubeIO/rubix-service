64e5e745acfe998759c97000001b3f08892bb6b8
enjuthulung <enjuthulung@gmail.com>
2021-04-30 11:38:03 +0545

    Add DATA_PUSH PythonApp

--- src/system/apps/python/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['bacnet_server_app', 'lora_raw_app', 'point_server_app', 'data_push_app']
-
-from src.system.apps.python import bacnet_server_app
-from src.system.apps.python import lora_raw_app
-from src.system.apps.python import point_server_app
-from src.system.apps.python import data_push_app
