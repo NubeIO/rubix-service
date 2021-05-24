beb2463406b0d658268f6385df0c912aa977e8f6
RaiBnod <binodrayee@gmail.com>
2021-05-24 12:38:20 +0545

    Don't restrict install when it's DOWNLOADED

--- src/system/resources/app/install.py
+++ src/system/resources/app/install.py
@@ -18,8 +18,6 @@ class InstallResource(RubixResource):
         download_state: str = get_download_state().get('state')
         if download_state == DownloadState.DOWNLOADING.name:
             raise PreConditionException('Download is in progress')
-        elif download_state == DownloadState.DOWNLOADED.name:
-            raise PreConditionException('Download state is not cleared')
         install_res = []
         rubix_plat = args.pop(next((i for i, item in enumerate(args) if item["service"].upper() == "RUBIX_PLAT"), -1))
         processes = []
