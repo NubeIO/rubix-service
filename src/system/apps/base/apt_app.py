6b92bf26b18ec533beb19388eec488c2b314e160
RaiBnod <binodrayee@gmail.com>
2021-12-01 08:52:21 +0545

    Fix: Open VPN installation

--- src/system/apps/base/apt_app.py
+++ src/system/apps/base/apt_app.py
@@ -1,6 +1,5 @@
 from abc import ABC
 
-import gevent
 from flask import current_app
 from werkzeug.local import LocalProxy
 
@@ -22,19 +21,19 @@ class AptApp(InstallableApp, ABC):
 
     def install(self) -> bool:
         logger.info('Hitting apt-get update...')
-        execute_command('sudo apt-get update')
+        execute_command('apt-get update')
         logger.info('Installing OpenVPN Service...')
-        if not execute_command('sudo apt-get install openvpn'):
+        if not execute_command('apt-get install openvpn -y'):
             return False
         logger.info('Successfully installed OpenVPN service')
         return True
 
     def uninstall(self) -> bool:
         logger.info('Stopping OpenVPN Service...')
-        if not execute_command('sudo systemctl disable openvpn@client && sudo systemctl stop openvpn@client'):
+        if not execute_command('systemctl disable openvpn@client && systemctl stop openvpn@client'):
             return False
         logger.info('Removing OpenVPN service...')
-        if not execute_command('sudo apt-get remove openvpn'):
+        if not execute_command('apt-get remove openvpn'):
             return False
         logger.info('OpenVPN Service is deleted.')
         return True
