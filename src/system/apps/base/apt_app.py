26db542b9c46fdb68a68fbdc180596f7dd11bdf2
RaiBnod <binodrayee@gmail.com>
2022-01-04 18:53:54 +0545

    OpenVPN enable itself on installation plus allow-releaseinfo-change

--- src/system/apps/base/apt_app.py
+++ src/system/apps/base/apt_app.py
@@ -21,11 +21,13 @@ class AptApp(InstallableApp, ABC):
 
     def install(self) -> bool:
         logger.info('Hitting apt-get update...')
-        execute_command('apt-get update')
+        execute_command('apt-get update --allow-releaseinfo-change')
         logger.info('Installing OpenVPN Service...')
         if not execute_command('apt-get install openvpn -y'):
             return False
         logger.info('Successfully installed OpenVPN service')
+        if not execute_command('systemctl disable openvpn@client'):
+            return False
         return True
 
     def uninstall(self) -> bool:
