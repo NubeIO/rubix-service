bcb7b12c2bb4e45bb303489c8ffd5936536f9b70
enjuthulung <enjuthulung@gmail.com>
2022-03-02 17:40:35 +0545

    Add comment and tags endpoint

--- src/slaves/resources/slaves_singular.py
+++ src/slaves/resources/slaves_singular.py
@@ -1,6 +1,8 @@
 import json
 
-from rubix_http.exceptions.exception import NotFoundException
+from flask import request
+from flask_restful import reqparse
+from rubix_http.exceptions.exception import NotFoundException, BadDataException
 
 from src.slaves.resources.slaves_base import SlavesBase
 from src.system.utils.file import write_file
@@ -16,3 +18,35 @@ class SlavesSingular(SlavesBase):
         del slaves[global_uuid]
         write_file(slaves_file, json.dumps(slaves))
         return slaves
+
+
+class SlavesComment(SlavesBase):
+
+    @classmethod
+    def put(cls, global_uuid):
+        parser = reqparse.RequestParser()
+        parser.add_argument('comment', type=str, required=True, store_missing=False)
+        data = parser.parse_args()
+        comment = data.get("comment")
+        return cls.update_slave_comment(global_uuid, comment)
+
+    @classmethod
+    def delete(cls, global_uuid):
+        cls.update_slave_comment(global_uuid, "")
+        return '', 204
+
+
+class SlavesTags(SlavesBase):
+
+    @classmethod
+    def put(cls, global_uuid):
+        args = request.get_json()
+        tags = args.get("tags")
+        if not isinstance(tags, list):
+            raise BadDataException('Invalid request')
+        return cls.update_slave_tags(global_uuid, tags)
+
+    @classmethod
+    def delete(cls, global_uuid):
+        cls.update_slave_tags(global_uuid, [])
+        return '', 204
