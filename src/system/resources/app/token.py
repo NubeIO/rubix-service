d387a1cf06ed38759dd71491f32639f8be0f8daa
RaiBnod <binodrayee@gmail.com>
2022-04-08 12:49:38 +0545

    Improvement: Too many open files

--- src/system/resources/app/token.py
+++ src/system/resources/app/token.py
@@ -3,21 +3,24 @@ from registry.models.model_github_info import GitHubInfoModel
 from registry.resources.resource_github_info import get_github_info, put_github_info
 from rubix_http.resource import RubixResource
 
+github_info_model: [GitHubInfoModel, None] = get_github_info()
+
 
 class TokenResource(RubixResource):
 
     @classmethod
     def get(cls):
-        github_info_model: [GitHubInfoModel, None] = get_github_info()
         if not github_info_model:
             abort(404, message="GitHub token doesn't exist")
         return {"token": f"{github_info_model.token[:4]}***{github_info_model.token[-4:]}"}
 
     @classmethod
     def put(cls):
+        global github_info_model
         parser = reqparse.RequestParser()
         parser.add_argument('token', type=str)
         args = parser.parse_args()
         token: str = args['token']
         put_github_info(GitHubInfoModel(token=token))
+        github_info_model = get_github_info()
         return {'token': get_github_info().token}
